e146bdbfd98763d3b942fdc4117370cdd78914e9
Lina Ebeid <linaebe0@gmail.com>
2020-05-12 17:32:02 +0300

    task queries added

---
+++ server/database/queries/task.js
@@ -0,0 +1,15 @@
+const Task = require('../modals/Tasks');
+
+const taskQueries = {};
+
+taskQueries.createTask = ({ userID, title, description, category, time }) =>
+  Task.create({ userID, title, description, category, time });
+
+taskQueries.deleteTask = (id) => Task.findByIdAndDelete(id);
+
+taskQueries.findTasksByUserID = (userID) => Task.find({ userID });
+
+taskQueries.editTask = (id, { title, description, category, time }) =>
+  Task.findByIdAndUpdate(id, { title, description, category, time });
+
+module.exports = { taskQueries };
