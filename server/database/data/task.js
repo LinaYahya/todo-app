4a32c09e4bddb753dba61d807d167150a5dfc46a
Lina Ebeid <linaebe0@gmail.com>
2020-05-13 15:48:33 +0300

    fakedata fixed to console data

--- server/database/data/task.js
+++ server/database/data/task.js
@@ -1,40 +1,45 @@
 const Task = require('../modals/Tasks');
 
-Task.create([
-  {
-    userID: '12b',
-    title: 'meeting',
-    description: 'clients meeting',
-    category: 'work',
-    time: Date.now(),
-  },
-  {
-    userID: '12b',
-    title: 'dinner',
-    description: 'dinner with friends',
-    category: 'pesonal',
-    time: new Date('June 17, 2020 03:30:00'),
-  },
-  {
-    userID: '12b',
-    title: 'trip',
-    description: 'trip with family',
-    category: 'personal',
-    time: new Date('June 18, 2020 03:30:00'),
-  },
-  {
-    userID: '15cc',
-    title: 'do laundry',
-    category: 'personal',
-    time: new Date('June 17, 2020 05:30:00'),
-  },
-  {
-    userID: '15cc',
-    title: 'class english',
-    description: 'class english to the new students',
-    category: 'work',
-    time: new Date('June 17, 2020 03:30:00'),
-  },
-]);
+Task.create(
+  [
+    {
+      userID: '12b',
+      title: 'meeting',
+      description: 'clients meeting',
+      category: 'work',
+      time: Date.now(),
+    },
+    {
+      userID: '12b',
+      title: 'dinner',
+      description: 'dinner with friends',
+      category: 'pesonal',
+      time: new Date('June 17, 2020 03:30:00'),
+    },
+    {
+      userID: '12b',
+      title: 'trip',
+      description: 'trip with family',
+      category: 'personal',
+      time: new Date('June 18, 2020 03:30:00'),
+    },
+    {
+      userID: '15cc',
+      title: 'do laundry',
+      category: 'personal',
+      time: new Date('June 17, 2020 05:30:00'),
+    },
+    {
+      userID: '15cc',
+      title: 'class english',
+      description: 'class english to the new students',
+      category: 'work',
+      time: new Date('June 17, 2020 03:30:00'),
+    },
+  ],
+  (err, res) => {
+    console.log('hiiiii', res);
+  }
+);
 
 module.exports = Task;
