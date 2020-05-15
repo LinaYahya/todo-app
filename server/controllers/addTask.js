ef55add7c259a88902efd4fb38d3603c88bf3db4
Lina Ebeid <linaebe0@gmail.com>
2020-05-15 18:25:52 +0300

    create task

--- server/controllers/addTask.js
+++ server/controllers/addTask.js
@@ -1,4 +1,6 @@
 const taskSchema = require('./validation/taskSchema');
+const { taskQueries } = require('../database/queries/task');
+const Task = require('../database/modals/Tasks');
 
 module.exports = async (req, res, next) => {
   const { title, description, category, time } = req.body;
@@ -10,7 +12,17 @@ module.exports = async (req, res, next) => {
     if (validation.error) {
       throw validation.error.details;
     }
+    const a = await Task.create({
+      userID: 'ab123',
+      title,
+      description,
+      category,
+      time,
+    });
+    console.log('hi task', a);
+    // console.log(Task.find({userID:'ab123'}));
+    // res.json("Task.find({userID:'ab123'})");
   } catch (err) {
-    next(err);
+    console.log(err);
   }
 };
