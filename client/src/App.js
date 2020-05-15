59c735d0db3b0dbf84d3de297815581bb055156e
Lina Ebeid <linaebe0@gmail.com>
2020-05-15 15:44:35 +0300

    add google login button at login component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Googlelogin from './components/login';
 
 function App() {
   return (
     <div className="App">
+      <Googlelogin />
     </div>
   );
 }
