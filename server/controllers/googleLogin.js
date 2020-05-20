8a8389462d470f173b218aa8821ac54706ed132f
Lina Ebeid <linaebe0@gmail.com>
2020-05-20 14:44:17 +0300

    name sent as token

--- server/controllers/googleLogin.js
+++ server/controllers/googleLogin.js
@@ -8,15 +8,13 @@ require('env2')('config.env');
 
 module.exports = async (req, res, next) => {
   const { tokenId, googleId } = req.body;
-
-  const token = sign({ id: googleId }, process.env.SECRET_KEY);
-  res.cookie('token', token);
-
   const idInfo = await axios.get(
     `https://oauth2.googleapis.com/tokeninfo?id_token=${tokenId}`
   );
   const { name } = idInfo.data;
 
+  const token = sign({ id: googleId, name }, process.env.SECRET_KEY);
+  res.cookie('token', token);
+
   res.end();
-  // verify().catch(console.error);
 };
