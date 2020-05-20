017f899e0b56293e22d3ee277c78e1b79c402b4a
Lina Ebeid <linaebe0@gmail.com>
2020-05-20 12:18:46 +0300

    add clientId and modifiy success response

---
+++ client/src/component/login.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { GoogleLogin } from 'react-google-login';
+import '../App.css';
+
+const successResponse = ({ tokenId }) => {
+ console.log(tokenId, 'hi token')
+  fetch.post('api/v1/login/google', {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(tokenId)
+  })
+    .then((res) => res.json())
+    .then(console.log)
+    .catch(console.log);
+};
+
+const failureResponse = (response) => {
+	console.log('error', response);
+};
+
+function Googlelogin() {
+  return (
+    <div className="logincontainer">
+      <GoogleLogin
+        clientId="350966115127-cs3q5j21nlacr1hg3njv3v8co74q23ab.apps.googleusercontent.com"
+        className="login-btn"
+        onSuccess={successResponse}
+        onFailure={failureResponse}
+        cookiePolicy={'single_host_origin'}
+        isSignedIn={true}
+
+      />
+    </div>
+  )
+}
+
+export default Googlelogin;
+
+
