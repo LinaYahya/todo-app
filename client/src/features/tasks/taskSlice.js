b6ab45ef4ce352e11107fce9875971f3d1594c42
Lina Ebeid <linaebe0@gmail.com>
2020-10-11 23:11:47 +0300

    fetch tasks at taskSlice

--- client/src/features/tasks/taskSlice.js
+++ client/src/features/tasks/taskSlice.js
@@ -1,12 +1,33 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+// import { fetchPosts } from "../../../../../../redux/redux-essentials-example-app/src/features/posts/postsSlice";
 
+export const fetchTasks = createAsyncThunk("tasks/fetchTasks", async () => {
+  const data = await fetch("/api/v1/task");
+  const tasks = await data.json();
+  return tasks;
+});
+const initialState = {
+  status: "idle",
+  error: null,
+  tasks: [],
+};
 
 const taskSlice = createSlice({
   name: "tasks",
-  initialState: [],
-  reducers: {
-
-  }
-})
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [fetchTasks.pending]: (state, action) => {
+      state.status = "loading";
+    },
+    [fetchTasks.fulfilled]: (state, action) => {
+      state.tasks = state.tasks.concat(action.payload);
+    },
+    [fetchTasks.rejected]: (state, action) => {
+      state.status = "failed";
+      state.error = action.error.message;
+    },
+  },
+});
 
 export default taskSlice.reducer;
